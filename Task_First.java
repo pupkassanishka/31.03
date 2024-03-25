010357a86965d846b9b68a7dd286c62c0c6e3abc
pupkassanishka <162048846+pupkassanishka@users.noreply.github.com>
2024-03-25 21:23:10 +0300

    1.1

---
+++ Task_First.java
@@ -0,0 +1,7 @@
+public class Task_First {
+    public static void main(String[] args) {
+        String x = "Мама мыла раму";
+        String y = "Мама" + " " + "мыла" + " " + "раму";
+      System.out.println(x==y);
+    }
+}
