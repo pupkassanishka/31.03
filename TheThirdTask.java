670cff6c7393d2ab5f949a00bd71c95ccbbc6595
pupkassanishka <162048846+pupkassanishka@users.noreply.github.com>
2024-03-25 21:35:57 +0300

    1.3

---
+++ TheThirdTask.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TheThirdTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = scanner.nextLine();
+        System.out.println(str);
+
+    }
+}
