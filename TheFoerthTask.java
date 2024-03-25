d6f5e6e53a1d37e9c41c5ef8a517063302aeb201
pupkassanishka <162048846+pupkassanishka@users.noreply.github.com>
2024-03-25 21:44:57 +0300

    1.4

---
+++ TheFoerthTask.java
@@ -0,0 +1,7 @@
+public class TheFoerthTask {
+    public static void main(String[] args) {
+        String str =  "012312";
+        Long num = Long.parseLong(str);
+        System.out.println(str);
+    }
+}
